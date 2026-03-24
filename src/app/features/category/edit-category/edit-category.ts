bec63ec7f97a74d9d1255369a6840ae0dd975257
Mateusz Pelczar <matrix1555555@gmail.com>
2026-03-25 00:36:29 +0100

    Added category list

---
+++ src/app/features/category/edit-category/edit-category.ts
@@ -0,0 +1,11 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-category',
+  imports: [],
+  templateUrl: './edit-category.html',
+  styleUrl: './edit-category.css',
+})
+export class EditCategory {
+  id = input<string>();
+}
